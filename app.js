e9e893a35dcbb9c5a38b44cd0331c4a250901852
Ben Edwards <benguin2kxx@gmail.com>
2016-09-15 23:02:26 -0400

    Update for some col text showing

--- app.js
+++ app.js
@@ -13,7 +13,15 @@ var puzzle1 = [
 
 var curPuzzle = puzzle1;
 
+var difficulty = 'easy';
+
 var numErrors = 0;
+var maxErrors = 5;
+if (difficulty == 'medium') {
+  maxErrors = 3;
+} else if (difficulty == 'hard') {
+  maxErrors = 1;
+}
 
 // var inversePuzzle = function(puzzle) {
 //   for (row = 0; row < puzzle.length; row++) {
@@ -33,14 +41,14 @@ var getCurrentPuzzleLayout = function() {
   return curPuzzle;
 }
 
-var getGridTexts = function(puzzle) {
-  console.log("numCols: " + puzzle.length);
+var getGridPuzzleNums = function(puzzle) {
+  // console.log("numCols: " + puzzle.length);
   var gridTexts = new Array();
   for (var row = 0; row < puzzle.length; row++) {
     // console.log("curPuzzle[col]: "
     //   + curPuzzle[col]);
 
-    var r = getTextFromGivenRow(puzzle[row]);
+    var r = getNumsFromGivenRow(puzzle[row]);
     // console.log("getTextFromGivenRow(puzzle[row]): " + r);
     gridTexts.push(r);
     // var givenCol = new Array();
@@ -55,8 +63,8 @@ var getGridTexts = function(puzzle) {
   return gridTexts;
 }
 
-var getTextFromGivenRow = function(row) {
-  var t = "";
+var getNumsFromGivenRow = function(row) {
+  var t = new Array();
   var c = 0;
   // console.log("row: " + row);
   // console.log("row l: " + row.length);
@@ -71,7 +79,8 @@ var getTextFromGivenRow = function(row) {
       c++;
     } else {
       if (c != null && c != 0) {
-        t += (" " + c + " ");
+        // t += (c + ' ');
+        t.push(c);
         c = 0;
       }
     }
@@ -92,7 +101,7 @@ var printGrid = function(grid) {
   }
 }
 
-var clickCell = function() {
+var clickCell = function(e) {
   // console.log("cell clicked (" + this.row + ", " + this.col + ")");
   // console.log("is mouse down? " + mouseDown);
   // if (mouseDown) {
@@ -108,10 +117,17 @@ var clickCell = function() {
     }
   } else {
     if (this.state == 0) {
+      e.preventDefault();
+      // this.toggle("highlight");
+      // this.animate({backgroundColor: '#FFFFFF'});
+      // this.className += ' highlight';
       this.style.background = "red";
       this.style.border = "solid grey 1px";
-      this.state = 2;
+      this.state = 0;
       numErrors++;
+      console.log("numErrors: " + numErrors);
+      if (numErrors == maxErrors)
+        gameOver();
     }
     // else {
     //   this.style.background = "white";
@@ -129,7 +145,8 @@ var isCellClickValid = function(cell) {
   return false;
 }
 
-var initGrid = function(grid) {
+var initGrid = function(grid, colNums, rowNums) {
+  initTopRow(grid, colNums);
   for (var row = 0; row < numRows; row++) {
     var rowDiv = document.createElement("div");
     // var rowDiv = document.getElementById("inforowdiv" + row);
@@ -143,7 +160,7 @@ var initGrid = function(grid) {
       colDiv.style.width = cellDim + "px";
       colDiv.style.height = cellDim + "px";
       colDiv.style.background = "white";
-      colDiv.style.color = "black";
+      // colDiv.style.color = "black";
       colDiv.style.border = "solid black 1px";
       colDiv.style.display = "inline-block";
 
@@ -154,6 +171,60 @@ var initGrid = function(grid) {
       rowDiv.appendChild(colDiv);
     }
   }
+}
+
+var initTopRow = function(grid, colNums, rowNums) {
+  var maxColInfos = getMaxLengthFromInfo(colNums);
+  console.log("max length: " + maxColInfos);
+  for (var i = 0; i < maxColInfos; i++) {
+    var rowDiv = document.createElement("div");
+    // rowDiv.style.border = "solid black 1px";
+    grid.appendChild(rowDiv);
+    var rowContainerDiv = document.createElement("div");
+    rowContainerDiv.style.background = "white";
+    // rowContainerDiv.style.border = "solid black 1px";
+    rowContainerDiv.style.display = "inline-block";
+    rowDiv.appendChild(rowContainerDiv);
+    var emptyDiv = document.createElement("div");
+    emptyDiv.style.width = cellDim + "px";
+    emptyDiv.style.height = cellDim + "px";
+    // emptyDiv.style.background = "white";
+    // emptyDiv.style.border = "solid black 1px";
+    emptyDiv.style.display = "inline-block";
+    rowContainerDiv.appendChild(emptyDiv);
+    for (var col = 0; col < colNums.length; col++) {
+      var colDiv = document.createElement("div");
+      colDiv.style.width  = cellDim + "px";
+      colDiv.style.height = cellDim + "px";
+      colDiv.style.background = "#ccc";
+      colDiv.style.color = "black";
+      // colDiv.style.border = "solid black 1px";
+      colDiv.style.display = "inline-block";
+      colDiv.style.textAlign = "center";
+      colDiv.style.verticalAlign = "bottom";
+      // colDiv.innerHTML.verticalAlign = "bottom";
+      // var str = "" + colNums[col];
+      // var splitStr = str.split(" ");
+
+      // colDiv.innerHTML = str.join(str.split(" "), '\n');
+      if (colNums[col][i])
+        colDiv.innerHTML = colNums[col][i];
+      rowContainerDiv.appendChild(colDiv);
+    }
+  } 
+}
+
+var getMaxLengthFromInfo = function(info) {
+  var max = 0;
+  for (var i = 0; i < info.length; i++) {
+    if (info[i].length > max) 
+      max = info[i].length;
+  }
+  return max;
+}
+
+var gameOver = function() {
+  console.log("game finished");
 }
 
 var initRowInfo = function(rowInfo, rowTexts) {
@@ -204,15 +275,16 @@ printGrid(getCurrentPuzzleLayout());
 
 // var rowTexts = new Array();
 
-var gridTexts = getGridTexts(curPuzzle);
-console.log("gridTexts: " + gridTexts);
+var gridNums = getGridPuzzleNums(curPuzzle);
+console.log("gridNums: " + gridNums);
 // console.log("cTexts: " + getRowTexts(curPuzzle));
 
-var rowTexts = gridTexts.slice(0, curPuzzle.length);
-var colTexts = gridTexts.slice(curPuzzle.length, gridTexts.length+1);
+var rowNums = gridNums.slice(0, curPuzzle.length);
+var colNums = gridNums.slice(curPuzzle.length, gridNums.length+1);
 
-console.log("rowTexts: " + rowTexts);
-console.log("colTexts: " + colTexts);
+colNums = [[1], [2,2], [3], [4], [5]];
+console.log("rowNums: " + rowNums);
+console.log("colNums: " + colNums);
 
 // var extra = document.getElementById("extras");
 // var colInfo = document.getElementById("colInfo");
@@ -222,4 +294,4 @@ var grid = document.getElementById("grid");
 
 // initRowInfo(grid, rowTexts);
 // initColInfo(colInfo, colTexts);
-initGrid(grid);
+initGrid(grid, colNums, rowNums);
